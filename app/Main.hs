7ec24d54e91ed17403e306245893f77e2f048ab4
kotfind <kotfind@yandex.ru>
2022-06-15 00:49:11 +0300

    Use argv, read code from file

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
 module Main where
 
 import Exec (exec)
+import System.IO (readFile)
+import System.Environment (getArgs, getProgName)
 
 main :: IO ()
-main = exec True "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
+main = do
+    cmd     <- getProgName
+    args    <- getArgs
+
+    let is_debug_flag = (`elem` ["--debug", "-d"])
+    let debug = any is_debug_flag args
+    let args' = filter (not . is_debug_flag) args
+
+    if length args' /= 1
+    then putStrLn $ "Usage: " ++ cmd ++ " [-d | --debug] filename"
+    else do
+        let filename:[] = args'
+        code <- readFile filename
+        exec debug code
